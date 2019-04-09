ed7b2351573ef64637c5731f9ff36327a6f7a54c
Tiago Margalho <tiago.margalho@trayport.com>
2019-04-09 17:08:34 +0100

    Read config from environment.

--- go.mod
+++ go.mod
@@ -3,11 +3,8 @@ module github.com/tiagomargalho/jaeger-test-client
 go 1.12
 
 require (
-	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
-	github.com/opentracing/opentracing-go v1.0.2
+	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pkg/errors v0.8.1 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	github.com/uber/jaeger-client-go v2.15.0+incompatible
-	github.com/uber/jaeger-lib v1.5.0
-	golang.org/x/net v0.0.0-20190313082753-5c2c250b6a70 // indirect
+	github.com/uber/jaeger-client-go v2.16.0+incompatible
+	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -4,32 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/opentracing/opentracing-go/ext"
 
 	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
 func main() {
 	interval := flag.Int("interval", 1000, "Interval (ms) at which traces should be generated.")
+
 	flag.Parse()
 	fmt.Printf("Generating traces every %d ms\n", *interval)
 
 	ticker := time.NewTicker(time.Millisecond * time.Duration(*interval))
 
-	cfg := jaegercfg.Configuration{
-		Sampler: &jaegercfg.SamplerConfig{
-			Type:  jaeger.SamplerTypeConst,
-			Param: 1,
-		},
-	}
-
-	closer, err := cfg.InitGlobalTracer("jaeger-test-client")
+	cfg, err := jaegercfg.FromEnv()
 
 	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Sending traces to JAEGER_ENDPOINT: %s\n", os.Getenv("JAEGER_ENDPOINT"))
+
+	closer, e := cfg.InitGlobalTracer("jaeger-test-client")
+
+	if e != nil {
 		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
 		return
 	}
